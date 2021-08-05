e5b8f0527d719b159dbbe2b7f23ff31d1732b2d1
Weinan Qiu <davidiamyou@gmail.com>
2021-08-05 09:59:02 +0800

    promplus module

---
+++ promplus/README.md
@@ -0,0 +1,27 @@
+# promplus
+
+Out-of-box HTTP middlewares to collect common prometheus metrics
+
+```shell
+go get -u github.com/absurdlab/pkg/promplus
+```
+
+## Metrics
+
+- :watch: handler duration
+- :ledger: handler visit count
+- :ok: handler response status count
+
+## Usage
+
+```go
+target := &promplus.Target{
+    Namespace: "my_company",
+    Service: "my_app",
+}
+
+var h = myHandler()
+h = target.Duration()(h)
+h = target.VisitCount()(h)
+h = target.StatusCount()(h)
+```
--- promplus/visit_test.go
+++ promplus/visit_test.go
@@ -10,6 +10,55 @@ import (
 	"testing"
 )
 
+func TestDurationMetric(t *testing.T) {
+	target := &promplus.Target{
+		Namespace: "promplus",
+		Service:   "test",
+	}
+
+	var h http.Handler = http.HandlerFunc(internal.Handle)
+	h = target.Duration()(h)
+
+	r := httptest.NewRequest(http.MethodGet, "/test?duration=1", nil)
+	rr := httptest.NewRecorder()
+
+	h.ServeHTTP(rr, r)
+
+	if got, want := testutil.CollectAndCount(target.DurationMetric()), 1; got != want {
+		t.Errorf("expect %d, get %d", want, got)
+	}
+}
+
+func TestStatusCountMetric(t *testing.T) {
+	target := &promplus.Target{
+		Namespace: "promplus",
+		Service:   "test",
+	}
+
+	var h http.Handler = http.HandlerFunc(internal.Handle)
+	h = target.StatusCount()(h)
+
+	r1 := httptest.NewRequest(http.MethodGet, "/test?status=200", nil)
+	r2 := httptest.NewRequest(http.MethodGet, "/test?status=400", nil)
+	rr := httptest.NewRecorder()
+
+	h.ServeHTTP(rr, r1)
+	h.ServeHTTP(rr, r2)
+	h.ServeHTTP(rr, r1)
+
+	expected := `
+		# HELP promplus_test_http_response_status Status of HTTP response
+        # TYPE promplus_test_http_response_status counter
+        promplus_test_http_response_status{method="GET",path="/test",service="test",status="200"} 2
+        promplus_test_http_response_status{method="GET",path="/test",service="test",status="400"} 1
+`
+
+	err := testutil.CollectAndCompare(target.StatusCountMetric(), strings.NewReader(expected))
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 func TestVisitCountMetric(t *testing.T) {
 	target := &promplus.Target{
 		Namespace: "promplus",
